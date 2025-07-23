8e5205d887727929faca883c83a9d70e4b0bb734
lucasbatistaf <lucaasbatista182@gmail.com>
2025-07-23 10:34:57 -0300

    start creating the staging sql

---
+++ indicium_challenge/models/staging/products/stg_products__suppliers.sql
@@ -0,0 +1,2 @@
+SELECT *
+FROM {{ source('PRODUCTS', 'SUPPLIERS') }}
---
+++ logs/query_log.sql
@@ -0,0 +1,5 @@
+-- created_at: 2025-07-22T00:23:39.289419003+00:00
+-- dialect: snowflake
+-- node_id: not available
+-- desc: not provided
+select 1 as id;
